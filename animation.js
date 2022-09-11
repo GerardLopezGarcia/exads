3e6e97360ba75d84f736b1750bbcf118ce550a7b
Gerard_López_garcía <72072309+GerardLopezGarcia@users.noreply.github.com>
2022-09-11 10:58:53 +0200

    Delete animation.js

--- animation.js
+++
@@ -1,8 +0,0 @@
-// avoid dragging img & texts
-$('img,h4,h1,h6,h3').on('dragstart', function(event) { event.preventDefault(); });
-//animation
-const web_init = gsap.timeline({default:{ease:'powe4.inOut'}})
-
-web_init.from('.anim_1',{y:35,opacity:0,stagger:0.2});
-
-
